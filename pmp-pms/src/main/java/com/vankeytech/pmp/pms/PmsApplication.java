2ac47b1e52102e7ed8b19572772f86f9f4c4b6b5
huanghao <huanghao@vankeytech.com>
2017-11-18 16:05:04 +0800

    配置修改

--- pmp-pms/src/main/java/com/vankeytech/pmp/pms/PmsApplication.java
+++ pmp-pms/src/main/java/com/vankeytech/pmp/pms/PmsApplication.java
@@ -1,9 +1,12 @@
 package com.vankeytech.pmp.pms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class PmsApplication {
 
 	public static void main(String[] args) {
