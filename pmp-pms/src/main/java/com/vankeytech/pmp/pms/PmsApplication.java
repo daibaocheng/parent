fa4d697977ddfeaa71be6074bf9f9acde4645af1
daibaocheng <991623692@qq.com>
2017-11-18 17:05:40 +0800

    pms

--- pmp-pms/src/main/java/com/vankeytech/pmp/pms/PmsApplication.java
+++ pmp-pms/src/main/java/com/vankeytech/pmp/pms/PmsApplication.java
@@ -1,12 +1,16 @@
 package com.vankeytech.pmp.pms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@MapperScan(basePackages = "com.vankeytech.pmp.pms.mapper")
+//@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+//@ImportResource(locations = "classpath:dubbo-consumer.xml")
 public class PmsApplication {
 
 	public static void main(String[] args) {
---
+++ pmp-pms/src/main/java/test/com/vankeytech/pmp/pms/controller/ProvinceComtrollerTest.java
@@ -0,0 +1,22 @@
+package test.com.vankeytech.pmp.pms.controller; 
+
+
+import com.vankeytech.pmp.pms.intf.PmsProvinceService;
+import org.junit.Test;
+import org.junit.After;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Scanner;
+
+/** 
+* ProvinceComtroller Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ʮһ�� 17, 2017</pre> 
+* @version 1.0 
+*/ 
+public class ProvinceComtrollerTest {
+
+
+
+} 
